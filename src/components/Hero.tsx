642f380598353f7adc4c9b1ff03a1cb6410d07e2
Bojidar Belov <bbelov.mmtb-m2025@fh-salzburg.ac.at>
2026-07-04 16:55:16 +0300

    hero section added

---
+++ src/components/Hero.tsx
@@ -0,0 +1,15 @@
+import './hero.css';
+import heroLogo from './../assets/hero_biking.webp'
+import Button from './button/Button';
+
+export default function Hero() {
+    return(
+        <div className='hero'>
+            <h1 className='heroTitle'>SHARE THE MOMENT</h1>
+            <p className='heroDesc'> Join bike trips with people who share your route or interests. Discover the <br /> 
+                joy of cycling in a safe and welcoming community.</p>
+            <div className='heroButton'><Button/></div>
+            <img className='heroImg' src={heroLogo}/>
+        </div>
+    )
+}
