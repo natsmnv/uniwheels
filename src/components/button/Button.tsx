45b93aa426e08b8a2ffae2490286486ff5fbfb55
Bojidar Belov <bojidarb.ac@gmail.com>
2026-07-05 22:41:59 +0300

    buttons fixed

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,9 +1,15 @@
 import './button.css';
 
 export default function Button() {
+    const handleClick = () => {
+        window.location.href = "https://fh-uni-wheels.projects.ct.fh-salzburg.ac.at";
+    };
+
     return (
         <>
-            <button>Start Now</button>
+            <button onClick={handleClick}>
+                Start Now
+            </button>
         </>
-    )
+    );
 }
