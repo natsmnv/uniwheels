09be20dae0d4404d08a872f2ca6b88fa779e5116
Serhat Sergikaya <164571443+serhat-sergikaya@users.noreply.github.com>
2026-07-05 15:34:04 +0200

    feat/ html

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import ProofSection from "./components/ProofSection";
+import RepeatCTA from "./components/RepeatCTA";
 
 function App() {
   return (
     <>
       <ProofSection />
+      <RepeatCTA />
     </>
   );
 }
